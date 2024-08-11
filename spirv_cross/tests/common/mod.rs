3c11426a9055232e349c25dcf2a03e94512dbe88
chyyran <ronny@ronnychan.ca>
2024-08-11 00:23:27 -0400

    test: fix test alignments

--- spirv_cross/tests/common/mod.rs
+++ spirv_cross/tests/common/mod.rs
@@ -1,9 +1,23 @@
 #[allow(clippy::cast_ptr_alignment)]
 pub fn words_from_bytes(buf: &[u8]) -> &[u32] {
-    unsafe {
-        std::slice::from_raw_parts(
-            buf.as_ptr() as *const u32,
-            buf.len() / std::mem::size_of::<u32>(),
-        )
-    }
+    bytemuck::cast_slice(buf)
 }
+
+macro_rules! include_transmute {
+    ($file:expr) => {{
+        #[repr(C)]
+        pub struct AlignedAs<Align, Bytes: ?Sized> {
+            pub _align: [Align; 0],
+            pub bytes: Bytes,
+        }
+
+        static ALIGNED: &AlignedAs::<&[u32], [u8]> = &AlignedAs {
+            _align: [],
+            bytes: *include_bytes!($file),
+        };
+
+        &ALIGNED.bytes
+    }};
+}
+
+pub(crate) use include_transmute;
